01559e4194076d5ba70bd88b2678a40cfe32b7e5
Dwaraka Nath <dtsdwarak@hotmail.com>
2015-09-08 22:11:43 +0530

    first push. yay!

---
+++ TfIdf.cpp
@@ -0,0 +1,107 @@
+//g++ -std=c++11 TfIdf.cpp -lboost_filesystem -lboost_system -lboost_regex
+//#include "mpi.h"
+#include "boost/filesystem.hpp"
+#include <iostream>
+#include "tf.cpp"
+using namespace std;
+
+int main () {
+  
+  int no_of_files=0;
+  vector<string> name_of_files;
+  vector<vector<pair<string, int> >> res;
+  vector<pair<string, int> > temp;
+  for ( boost::filesystem::recursive_directory_iterator end, dir("/home/admin46/Downloads/datasets/sample");dir != end; ++dir ) {
+    std::cout << dir->path().string() << "\n"; 
+	temp=getKeywords(dir->path().string());
+	res.push_back(temp);
+	name_of_files.push_back(dir->path().filename().string());
+	no_of_files++;
+  }
+  int count;
+  int arr[no_of_files][no_of_files];
+  for(int i=0;i<no_of_files;i++){
+  	for(int j=0;j<no_of_files;j++){
+  		arr[i][j]=0;
+  	}
+  }
+
+  
+  float percent;
+  cout<<"what %age of keywords you want to match";
+  cin>>percent;
+  percent=percent/100;
+
+  
+  for(int i=0;i<res.size();i++){
+  	unordered_map<string,int> findkeys(res[i].begin(),res[i].end());
+  	for(int j=i+1;j<res.size();j++){
+  		count=0;
+  		float limit=res[j].size()*percent;
+  		for(int k=0;k<limit;k++){
+  			if(findkeys[res[j][k].first]==1){
+  				count++;
+  			}
+  		}
+  		arr[i][j]=count;
+  		arr[j][i]=count;
+  	}
+  }
+  
+
+  for(int i=0;i<no_of_files;i++){
+  	for(int j=0;j<no_of_files;j++){
+  		cout<<arr[i][j]<<"  ";
+  	}
+  	cout<<endl;
+  }
+
+  for(int i=0;i<name_of_files.size();i++){
+  	cout<<name_of_files[i]<<endl;
+  }
+
+  cout<<"enter the name of the file";
+  string name;
+  int pos;
+  cin>>name;
+
+  for(int i=0;i<name_of_files.size();i++){
+  	if(name.compare(name_of_files[i])==0){
+  		pos=i;
+  		break;
+  	}
+  }
+
+  for(int i=0;i<no_of_files;i++){
+  	if(arr[pos][i]!=0){
+  		cout<<name_of_files[i]<<" ";
+  	}
+  }
+  cout<<endl;
+  cout<<"enter the file name for which you want to find transitive closure";
+  string fname;
+  cin>>fname;
+
+  for(int i=0;i<name_of_files.size();i++){
+  	if(name.compare(name_of_files[i])==0){
+  		pos=i;
+  		break;
+  	}
+  }
+  cout<<endl<<"the transitive closure of the file is";
+  for(int i=0;i<no_of_files;i++){
+  	if(arr[pos][i]!=0){
+	  		cout<<name_of_files[i]<<" ";
+	  		continue;
+	  	}
+  	for(int j=0;j<no_of_files;j++){
+	  	if(arr[pos][j]!=0&&arr[j][i]!=0){
+	  		cout<<name_of_files[i]<<" ";
+	  		break;
+	  	}
+	  }
+  }
+  cout<<endl;
+
+  return 0;
+}
---
+++ tf.cpp
@@ -0,0 +1,84 @@
+//g++ -std=c++11 -I /usr/lib/boost/include -L /usr/lib/boost/lib tfidf.cpp -lboost_regex
+// If you've included boost libraries in LD_LIBRARY_PATH, just use $ g++ -std=c++11 tfidf.cpp -lboost_regex
+
+#include <iostream>
+#include <string>
+#include <climits>
+#include <map>
+#include <unordered_map>
+#include <fstream>
+#include <algorithm>
+#include <boost/regex.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string/join.hpp>
+#include "stop_words.cpp"
+using namespace std;
+
+vector<pair<string, int> > getKeywords(string filename){
+	
+	string line,input_text="";
+  	ifstream myfile (filename);
+  	if (myfile.is_open())
+  	{
+    		while ( getline (myfile,line) )
+    		{
+			line = boost::regex_replace(line, boost::regex {"[^'a-zA-Z]"}, std::string {" "});
+    			line = boost::regex_replace(line, boost::regex {"[ \r\n\t]+"}, std::string {" "});
+    			transform(line.begin(), line.end(), line.begin(), ::tolower);     			
+			input_text+=line+" ";
+    		}
+    		myfile.close();
+  	}
+	vector<string> input_text_words;
+    boost::split(input_text_words, input_text, boost::is_any_of(" "));
+
+	map<string,int> found_keywords;
+	unordered_map<string, int> stop_words = get_stop_words();
+    int w_length,unique_keyword_count;
+
+    for(string& word : input_text_words)
+    {
+        boost::trim(word);
+
+        w_length = word.length();
+
+        if(w_length)
+        {
+
+            if(stop_words[word] == 0)
+            {
+                if(!found_keywords[word])
+                {
+                    found_keywords[word] = 1;
+                    unique_keyword_count++;
+                }
+                else
+                {
+
+                        found_keywords[word] += 1;
+                }
+
+            }
+        }
+    }
+
+	vector<pair<string, int> > found_keywords_sort(found_keywords.begin(), found_keywords.end());
+
+    struct
+    {
+        bool operator() (pair<string, int> a, pair<string, int> b)
+        {
+            return a.second > b.second;   // desc
+        }
+    } sort_keywords_desc;
+
+    sort(found_keywords_sort.begin(), found_keywords_sort.end(), sort_keywords_desc);
+	
+    for(int i=0;i<found_keywords_sort.size();i++){
+        cout<<found_keywords_sort[i].first<<" "<<found_keywords_sort[i].second<<endl;
+    }
+
+    return found_keywords_sort;
+
+}
+
